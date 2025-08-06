d04a36323f35f504ab439f944bcf4ef080e61124
Kashish Mittal <kmittal@redhat.com>
2025-08-06 14:59:34 -0400

    add 50 files
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- generate_catalog.py
+++ generate_catalog.py
@@ -1,8 +1,8 @@
 import os
 
-start_file_num = 59
-total_files = 50
-start_name_num = 55  # first name_num value
+start_file_num = 139
+total_files = 20
+start_name_num = 135  # first name_num value
 
 for i in range(total_files):
     file_num = start_file_num + i
